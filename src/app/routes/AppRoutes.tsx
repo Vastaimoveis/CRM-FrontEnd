a59f1c8cef865096f4463c43282affe1f6c9f206
Lorenzo-LP <lorenzocontato1@outlook.com>
2026-06-29 13:36:55 -0300

    new route login done

--- src/app/routes/AppRoutes.tsx
+++ src/app/routes/AppRoutes.tsx
@@ -18,6 +18,7 @@ function App() {
       <AppProviders>
         <Routes>
           <Route path='/' element={<Login />} />
+          <Route path='/login' element={<Login />} />
           <Route
             path="/funil"
             element={
